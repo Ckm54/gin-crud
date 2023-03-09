1d07e54a7974a8a6954b98ea7220bd1b8f5684a8
collo <mutaic84@gmail.com>
2023-03-09 16:04:37 +0300

    :hammer: update controller to create a single book

---
+++ controllers/books.go
@@ -0,0 +1,38 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/Ckm54/bookstore-go/models"
+	"github.com/gin-gonic/gin"
+)
+
+type CreateBookInput struct {
+	Title		string		`json:"title" binding:"required"`
+	Author	string		`json:"author" binding:"required"`
+}
+
+// get all books from database
+func FindBooks(ctx *gin.Context) {
+	var books []models.Book
+	models.DB.Find(&books)
+
+	ctx.JSON(http.StatusOK, gin.H{"data": books})
+}
+
+// create a new book
+func CreateBook(ctx *gin.Context) {
+	// validate inputs
+	var input CreateBookInput
+
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// create book
+	book := models.Book{Title: input.Title, Author: input.Author}
+	models.DB.Create(&book)
+
+	ctx.JSON(http.StatusOK, gin.H{"data": book})
+}
