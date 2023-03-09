120099185507de0e96972a49e1fea0112c75a9b3
collo <mutaic84@gmail.com>
2023-03-09 16:57:27 +0300

    :hammer: enable updating  a book's info

--- controllers/books.go
+++ controllers/books.go
@@ -12,6 +12,11 @@ type CreateBookInput struct {
 	Author	string		`json:"author" binding:"required"`
 }
 
+type UpdateBookInput struct {
+	Title		string 		`json:"title"`
+	Author	string		`json:"author"`
+}
+
 // get all books from database
 func FindBooks(ctx *gin.Context) {
 	var books []models.Book
@@ -44,6 +49,36 @@ func FindBook(ctx *gin.Context) {
 	if err := models.DB.Where("id = ?", ctx.Param("id")).First(&book).Error; err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Record Not Found!"})
 		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+// update a book's information
+func UpdateBook(ctx *gin.Context) {
+	// check if model exists
+	var book models.Book
+
+	if err := models.DB.Where("id = ?", ctx.Param("id")).First(&book).Error; err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	// validate input
+	var input UpdateBookInput
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// update the book's information in database
+	// walkaround to data update error
+	if input.Title != "" {
+		updateBook := models.Book{Title: input.Title}
+		models.DB.Model(&book).Updates(&updateBook)
+	} else if input.Author != "" {
+		updateBook := models.Book{Author: input.Author}
+		models.DB.Model(&book).Updates(&updateBook)
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{"data": book})
